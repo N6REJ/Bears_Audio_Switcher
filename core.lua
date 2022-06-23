e634c731d206e27b211e80ad615a1196a084de74
Bear <programming@hallhome.us>
2022-06-23 00:52:38 -0500

    Going back to our way of doing thing

--- core.lua
+++ core.lua
@@ -1,219 +1,73 @@
-local addon = LibStub("AceAddon-3.0"):NewAddon("BearsSwitcher", "AceConsole-3.0")
+BearsSwitcher = LibStub("AceAddon-3.0"):NewAddon("BearsSwitcher", "AceEvent-3.0", "AceConsole-3.0")
+local AC = LibStub("AceConfig-3.0")
+local ACD = LibStub("AceConfigDialog-3.0")
+local speaker1, speaker2
+local numDevices = Sound_GameSystem_GetNumOutputDrivers()
 
-local BearsSwitcherLDB =
-	LibStub("LibDataBroker-1.1"):NewDataObject(
-	"BearsSwitcher",
-	{
-		type = "data source",
-		text = "BearsSwitcher",
-		icon = "Interface/AddOns/Bears_Audio_Switcher/media/icon",
-		OnTooltipShow = function(tooltip)
-			local c = "Sound_OutputDriverIndex"
-			local m = GetCVar(c)
-			local n = Sound_GameSystem_GetNumOutputDrivers()
-			local t = Sound_GameSystem_GetOutputDriverNameByIndex(m)
-			tooltip:SetText("BearsSwitcher")
-			tooltip:AddLine("|cff0000ff" .. t .. "|r")
-			tooltip:AddLine(" ")
-			tooltip:AddLine("|cffddff00Left Click|r |cff00ff00= Toggle Audio Interfaces|r")
-			tooltip:AddLine("|cffddff00Right Click|r |cff00ff00= Configure|r")
-		end,
-		OnClick = function(a, b)
-			if b == "LeftButton" then
-				addon:switchOutputDriver()
-			elseif b == "RightButton" then
-				addon:BearsSwitcherConfig()
-			end
-		end
-	}
-)
-local icon = LibStub("LibDBIcon-1.0")
+function BearsSwitcher:OnInitialize()
+	-- uses the "Default" profile instead of character-specific profiles
+	-- https://www.wowace.com/projects/ace3/pages/api/ace-db-3-0
+	self.db = LibStub("AceDB-3.0"):New("BearsSwitcherDB", self.defaults, true)
 
-function formatText(t, c)
-	if c == nil then
-		return (t)
+	-- registers an options table and adds it to the Blizzard options window
+	-- https://www.wowace.com/projects/ace3/pages/api/ace-config-3-0
+	AC:RegisterOptionsTable("BearsSwitcher_Options", self.options)
+	self.optionsFrame = ACD:AddToBlizOptions("BearsSwitcher_Options", "Bears Audio Switcher")
+
+	-- https://www.wowace.com/projects/ace3/pages/api/ace-console-3-0
+	self:RegisterChatCommand("bs", "SlashCommand")
+	self:RegisterChatCommand("BearsSwitcher", "SlashCommand")
+
+	-- Get profile settings
+end
+
+-- Get listing of audio devices available
+function BearsSwitcher:FindDevices(numDevices)
+	-- query audio devices and populate array.
+	for index = 0, numDevices - 1, 1 do
+		-- print(index, Sound_GameSystem_GetOutputDriverNameByIndex(index))
+		self.db.Devices = Sound_GameSystem_GetOutputDriverNameByIndex(index)
+		print(index, self.db.Devices)
 	end
-	return ("|cff" .. c .. t .. "|r")
 end
 
-function pChat(t, c)
-	print(formatText("[" .. addon.name .. "]", "00ff00"), formatText(t, c))
-end
-
-function addon:switchOutputDriver()
-	local devlist = {}
-	local activeDrivers = 0
-	local totalDrivers = Sound_GameSystem_GetNumOutputDrivers()
-	for i = 1, totalDrivers do
-		local d = Sound_GameSystem_GetOutputDriverNameByIndex(i)
-		if d == "None" then
-			d = "System Default"
-		end
-		n = _cleanDevName(d)
-		devlist[i] = {}
-		devlist[i].name = n
-		devlist[i].active = self.db.global.devlist[n]
-		if devlist[i].active then
-			activeDrivers = activeDrivers + 1
-		end
-		devlist[i].desc = d
-	end
-	if activeDrivers == 0 then
-		pChat("No eligible devices selected in configuration", "dc322f")
-		return
-	end
-
-	local cVar = "Sound_OutputDriverIndex"
-	local curIndex = tonumber(GetCVar(cVar))
-	local lastIndex = curIndex
-	repeat
-		curIndex = curIndex + 1
-		if curIndex > totalDrivers then
-			curIndex = 1
-		end
-	until (devlist[curIndex].active == true)
-
-	if lastIndex == curIndex then
-		pChat("No change was made", "b58900")
-		return
-	end
-	SetCVar(cVar, curIndex)
-	AudioOptionsFrame_AudioRestart()
-	pChat("Output Device set to " .. formatText(devlist[curIndex].name, "268bd2"))
-end
-
-function _cleanDevName(name)
-	if string.find(name, "[(]") then
-		name = string.match(name, "^(.-)[(].*$")
-	end
-	return name:match "^%s*(.-)%s*$"
-end
-
-function addon:getDeviceSelectedState(info)
-	--print(info.option.name,':',self.db.global.devlist[info.option.name])
-	return (self.db.global.devlist[info.option.name])
-end
-
-function addon:setDeviceSelectedState(info, val)
-	self.db.global.devlist[info.option.name] = val
-end
-
-function addon:OnInitialize()
-	local defaults = {
-		profile = {
-			minimap = {
-				hide = false
-			},
-			devlist = {},
-			switch = {"NUMPADPLUS"}
-		},
-		global = {
-			devlist = {},
-			-- Make Keybind variable name Global
-			switch = {"NUMPADPLUS"}
-		}
-	}
-	local options = {
-		name = "BearsSwitcher",
-		handler = addon,
-		type = "group",
-		args = {
-			map = {
-				name = "Show Minimap Icon",
-				desc = "Show/Hide minimap Icon",
-				type = "toggle",
-				set = function(info, val)
-					addon:SetIconHidden(not val)
-				end,
-				get = function(info)
-					return not addon:IsIconHidden()
-				end
-			},
-			config = {
-				name = "Sound Devices",
-				desc = "Check the devices you want to cycle through when toggling devices",
-				type = "group",
-				args = {}
-			}
-		}
-	}
-	for i = 1, Sound_GameSystem_GetNumOutputDrivers() do
-		local dev = Sound_GameSystem_GetOutputDriverNameByIndex(i)
-		if dev == "None" then
-			dev = "System Default"
-		end
-		options.args.config.args["dev" .. i] = {
-			name = _cleanDevName(dev),
-			desc = dev,
-			type = "toggle",
-			width = "full",
-			get = function(info)
-				return (addon:getDeviceSelectedState(info))
-			end,
-			set = function(info, val)
-				addon:setDeviceSelectedState(info, val)
-			end
-		}
-	end
-
-	-- Obviously you'll need a ## SavedVariables: BearsSwitcherDB line in your TOC, duh!
-	self.db = LibStub("AceDB-3.0"):New("BearsSwitcherDB", defaults, true)
-
-	for k, v in pairs(options.args.config.args) do
-		local d = options.args.config.args[k].name
-		--print ('-->'..d)
-		if self.db.global.devlist[d] == nil then
-			self.db.global.devlist[d] = true
-		end
-	end
-
-	local AceConfig = LibStub("AceConfig-3.0")
-	AceConfig:RegisterOptionsTable("BearsSwitcher", options, {"switcher", "bs"})
-
-	icon:Register("BearsSwitcher", BearsSwitcherLDB, self.db.profile.minimap)
-	self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("BearsSwitcher", "BearsSwitcher")
-	self:RegisterChatCommand("sm", "smCommand")
-end
-
-function addon:BearsSwitcherConfig()
-	InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
-end
-
-function BearsSwitcherHelp()
-	print("Bears Switcher Options")
-	print("  help - this message")
-	print("  show - show minimap button")
-	print("  hide - hide minimap button")
-	print("  config - open configuration dialog")
-end
-
-function addon:SetIconHidden(state)
-	self.db.profile.minimap.hide = state
-	if state then
-		icon:Hide("BearsSwitcher")
+-- Get devices chosen
+function BearsSwitcher:GetDevices()
+	-- Read from profile the devices they want to use and assign them to variables.
+	local x = tonumber(GetCVar("Sound_OutputDriverIndex"))
+	if x == 0 then
+		SetCVar("Sound_OutputDriverIndex", "3")
 	else
-		icon:Show("BearsSwitcher")
+		SetCVar("Sound_OutputDriverIndex", "0")
 	end
 end
 
-function addon:IsIconHidden()
-	if self.db.profile.minimap.hide == nil then
-		return false
-	elseif self.db.profile.minimap.hide == false then
-		return false
-	end
-	return true
-end
-
-function addon:smCommand(args)
-	local cmd = string.lower(args)
-	if cmd == "hide" then
-		addon:SetIconHidden(true)
-	elseif cmd == "show" then
-		addon:SetIconHidden(false)
-	elseif cmd == "config" then
-		addon:BearsSwitcherConfig()
+function BearsSwitcher:SlashCommand(input, editbox)
+	if input == "enable" then
+		self:Enable()
+		self:Print("Enabled.")
+	elseif input == "disable" then
+		-- unregisters all events and calls BearsSwitcher:OnDisable() if you defined that
+		self:Disable()
+		self:Print("Disabled.")
 	else
-		BearsSwitcherHelp()
+		--[[ or as a standalone window
+		if ACD.OpenFrames["BearsSwitcher_Options"] then
+			ACD:Close("BearsSwitcher_Options")
+		else
+			ACD:Open("BearsSwitcher_Options")
+		end
+		]]
+		-- Store choices
+		BearsSwitcher:FindDevices(numDevices)
+		-- We need to tell them what device is being used now.
+		self:Print("Audio Devices changed")
+
+		-- Make the device active.
+		Sound_GameSystem_RestartSoundSystem()
+
+		-- https://github.com/Stanzilla/WoWUIBugs/issues/89
+		InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
+		InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
 	end
 end
