a8636edfb7e65bd8f1bc210a9eb6ec95fce61739
Bear <programming@hallhome.us>
2022-06-23 22:08:34 -0500

    now prints speaker name on change

--- core.lua
+++ core.lua
@@ -26,25 +26,6 @@ function BearsSwitcher:OnInitialize()
 	print("Bears Audio Switcher loaded.  type options type /bs")
 end
 
--- Get devices chosen
-function BearsSwitcher:GetActive()
-	-- Read from profile the devices they want to use and assign them to variables.
-	local x = tonumber(GetCVar("Sound_OutputDriverIndex"))
-	if x == 0 then
-		SetCVar("Sound_OutputDriverIndex", "3")
-	else
-		SetCVar("Sound_OutputDriverIndex", "0")
-	end
-end
-
--- keybind routine
-local function GetKey(self, key)
-	-- Get the keybind here
-	if BearsSwitcher.db.profile.toggle == key then
-		print(self:GetName(), key)
-	end
-end
-
 function BearsSwitcher:SlashCommand(input, editbox)
 	if input == "enable" then
 		self:Enable()
@@ -78,6 +59,7 @@ f:SetScript(
 			local switch = tonumber(GetCVar(cVar))
 			local spkr1 = BearsSwitcher.db.profile.spkr1 - 1
 			local spkr2 = BearsSwitcher.db.profile.spkr2 - 1
+
 			-- print("spkr1=", BearsSwitcher.db.profile.spkr1)
 			-- print("spkr2=", BearsSwitcher.db.profile.spkr2)
 			-- print("switch is: ", switch)
@@ -89,11 +71,15 @@ f:SetScript(
 				SetCVar("Sound_OutputDriverIndex", spkr1)
 			end
 
-			-- We need to tell them what device is being used now.
-			print("Audio Devices changed.  Speaker ", GetCVar("Sound_OutputDriverIndex"), " active")
-
 			-- Make the device active.
 			AudioOptionsFrame_AudioRestart()
+
+			-- Find out current device so we can tell them what it is.
+			cVar = "Sound_OutputDriverIndex"
+			current = tonumber(GetCVar(cVar))
+			-- We need to tell them what device is being used now.
+			--		print("Audio Devices changed.  Speaker ", Sound_ChatSystem_GetOutputDriverNameByIndex(current), " active")
+			print("Audio Devices changed:  Speaker ", Sound_GameSystem_GetOutputDriverNameByIndex(current), " active")
 		end
 
 		self:SetPropagateKeyboardInput(true)
